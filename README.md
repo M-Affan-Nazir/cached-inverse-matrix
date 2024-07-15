1a63de7eadf0b574e6db71520b08c0a9469267e7
M-Affan-Nazir <m.affann80@hotmail.com>
2024-07-15 17:43:15 -0600

    F-2

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 - Save the inverse of the Matrix in the form of a cache; inside the Object itself
 - Saves computational cost due to recalculation of Inverse; since it is saved inside cache
 - Useful when contents of a matrix are not changing
+
+# How to Use:
+- Create Matrix using 'cacheMatrix()' function
+- Fill the matrix by using the 'set()' function
+- calculate the inverse using 'cacheSolve()' function
+- To extract Actual matrix, use 'get()' function
+- To extract Inverse, use 'getInverse()' function
+
+# Note:
+- To reference/call the function of the object, use '$'
+  i.e. matrixObject$set()
--- main.R
+++ main.R
@@ -1,10 +1,36 @@
-makeMatrix <- function(x=numeric()){
+makeCacheMatrix <- function(x=numeric()){
   inverse <- NULL
+  changed <- FALSE
   set <- function(y) {
     x <<- y  #x exists in parent environment, therefore '<<-' used
+    changed <<- TRUE
+    inverse <<- NULL
   }
   get <- function() return(x)
-  setInverse <- function(inv) { inverse <<- inv } #inverse outside scope of function 'setInverse'; therefore <<- used
+  
+  cacheSolve <- function() {    #Calculate Inverse
+    if (!is.null(inverse) && !changed) {  # Return cached inverse if valid
+      message("Returning cached inverse.")
+      return(inverse)
+    } 
+      if (det(x) == 0) {
+        print("Matrix is singular and cannot be inverted.")
+      }
+      inverse <<- solve(x)
+      changed <<- FALSE
+      return(inverse)
+  }
+  
   getInverse <- function() return(inverse)
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  list(set = set, get = get, cacheSolve = cacheSolve, getInverse = getInverse)
 }
+
+
+#Example Usage:
+m <- makeCacheMatrix()
+m$set(matrix(rnorm(25),5,5))
+x <- m$cacheSolve()
+#Main Matrix:
+print(m$get())
+#Inverse of Matrix
+print(m$getInverse())
