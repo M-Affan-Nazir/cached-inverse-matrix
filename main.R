6759ee09675a1b05574188f4d0fe2e291b2ea726
M-Affan-Nazir <m.affann80@hotmail.com>
2024-07-15 17:12:01 -0600

    Finish1

--- main.R
+++ main.R
@@ -3,4 +3,8 @@ makeMatrix <- function(x=numeric()){
   set <- function(y) {
     x <<- y  #x exists in parent environment, therefore '<<-' used
   }
+  get <- function() return(x)
+  setInverse <- function(inv) { inverse <<- inv } #inverse outside scope of function 'setInverse'; therefore <<- used
+  getInverse <- function() return(inverse)
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
